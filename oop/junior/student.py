145a092acc5dbc4b2d532ffa92cda2904c83ef5e
胡子豪 <wuqieor@outlook.com>
2020-12-25 21:21:45 +0800

    [面向对象编程]类和对象

--- oop/junior/student.py
+++ oop/junior/student.py
@@ -19,8 +19,18 @@ class Student(object):
     def print_score(self):
         print(f'{self.name}:{self.score}')
 
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
 
 michael = Student('Michael', 98)
 bob = Student('Bob', 81)
 michael.print_score()
+print(michael.get_grade())
 bob.print_score()
+print(bob.get_grade())
