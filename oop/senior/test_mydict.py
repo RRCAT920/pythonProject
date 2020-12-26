08e6ce49fb1d526d23746722df5abe49de46cbff
胡子豪 <wuqieor@outlook.com>
2020-12-26 19:53:58 +0800

    [错误、调试、测试]unittest单元测试

---
+++ oop/senior/test_mydict.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import unittest
+from unittest import TestCase
+from oop.senior.mydict import Dict
+
+
+class TestDict(TestCase):
+    def setUp(self) -> None:
+        print('setup...')
+
+    def tearDown(self) -> None:
+        print('teardown...')
+
+    def test_init(self):
+        d = Dict(a=1, b=2)
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 2)
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_key_error(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            foo = d['empty']
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertTrue(d['key'], 'value')
+
+    def test_attr_error(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            foo = d.empty
+
+
+if __name__ == '__main__':
+    unittest.main()
